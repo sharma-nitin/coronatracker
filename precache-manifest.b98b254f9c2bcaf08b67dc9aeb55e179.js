6ce2c8eb4463995aefd0b55c6b592c6de65c7de4
Nitin Sharma <nitin.g.sharma@capgemini.com>
2020-05-02 16:17:14 +0530

    Updates

---
+++ precache-manifest.b98b254f9c2bcaf08b67dc9aeb55e179.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2351564d46e67dc5d434a89337844a40",
+    "url": "/coronatracker/index.html"
+  },
+  {
+    "revision": "521496feef11565d7d04",
+    "url": "/coronatracker/static/css/main.73e3b6fc.chunk.css"
+  },
+  {
+    "revision": "0e55861bae3334a6c009",
+    "url": "/coronatracker/static/js/2.d642ad35.chunk.js"
+  },
+  {
+    "revision": "79e0ae5b885937d45e4c923db2172094",
+    "url": "/coronatracker/static/js/2.d642ad35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "521496feef11565d7d04",
+    "url": "/coronatracker/static/js/main.0bca05df.chunk.js"
+  },
+  {
+    "revision": "b574eae86052649020cd",
+    "url": "/coronatracker/static/js/runtime-main.4bd08d4a.js"
+  },
+  {
+    "revision": "7e5635a4ceaa92f9b825ee4b72b10ef7",
+    "url": "/coronatracker/static/media/bag.7e5635a4.svg"
+  },
+  {
+    "revision": "7df0a764c96a9c2a53130beb6e4fd5ac",
+    "url": "/coronatracker/static/media/recovery.7df0a764.svg"
+  },
+  {
+    "revision": "e8f4146a853fbef32f9fa3fd9d773ad6",
+    "url": "/coronatracker/static/media/skull.e8f4146a.svg"
+  },
+  {
+    "revision": "00ce86c1af0919cc20bc4bf953f91af1",
+    "url": "/coronatracker/static/media/virus.00ce86c1.svg"
+  }
+]);
